94a2b3fbfc2e1748b81009d05cad6de252797582
Som3a <som3a11202@gmail.com>
2024-07-22 05:29:28 +0300

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+// import localFont from "next/font/local";
+
+const inter = Inter({ subsets: ["latin"] });
+
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+// });
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | bugbook",
+    default: "bugbook",
+  },
+  description: "The social media app for powernerds",
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
